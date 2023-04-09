0a4fa636493121c8265964d9683554025d2b1496
Qquiqlerr <Leha.metlushko@bk.ru>
2023-04-09 10:38:52 +0300

    for Pypi

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="Goosu",
+    version='0.0',
+    install_requires = ['requests',
+                        'json',],
+    author_email='Leha.metlushko@bk.ru',
+
+)
